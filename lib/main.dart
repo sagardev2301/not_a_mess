c9a4e0705f80c4458dc1ee231da9f195bfbea687
Sagar Kumar <sagarkumar.dev2301@gmail.com>
2023-06-03 23:18:45 +0530

    Ignore this eg.dart files. Trying to solve some issue.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-// ...
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
